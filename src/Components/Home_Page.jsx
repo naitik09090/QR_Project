96f5619bfba84cdf180b1f991ade775bb4b30483
naitik09090 <naitikraiyani88@gmail.com>
2025-08-21 11:47:13 +0530

    Img_Error

--- src/Components/Home_Page.jsx
+++ src/Components/Home_Page.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { useState } from "react";
 import React from "react";
 import { Container } from "react-bootstrap";
